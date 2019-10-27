a7e9159031ea24ff5596e8bffb6ece42675b00ae
hotatekaoru <kaorupochi@gmail.com>
2019-10-27 17:49:33 +0900

    :package: kapt & room

--- CreateMonster/app/src/main/java/com/example/createmonster/app/CreaturemonApplication.kt
+++ CreateMonster/app/src/main/java/com/example/createmonster/app/CreaturemonApplication.kt
@@ -1,18 +1,17 @@
 package com.example.createmonster.app
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Application
 import androidx.room.Room
 import com.example.createmonster.model.room.CreatureDatabase
 
-class CreaturemonApplication : AppCompatActivity() {
+class CreaturemonApplication : Application() {
 
     companion object {
         lateinit var database: CreatureDatabase
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate() {
+        super.onCreate()
 
         database = Room.databaseBuilder(this, CreatureDatabase::class.java, "creature_database").build()
     }
